49aec52106af359a837d96c92fea855ada570c43
KATIN John <johnpkatin@gmail.com>
2025-11-30 22:35:35 +0000

    todo-list projet ...

--- Lesson-11-arrays-and-loops/11-todo-list.js
+++ Lesson-11-arrays-and-loops/11-todo-list.js
@@ -1,9 +1,35 @@
-const todoList = [];
+/*Process for creating websites: 
+  1. Save data
+  2. Generate the HTML
+  3. Make it interactive
+*/
+
+/*Algo for this code
+  1. Loop through the array
+  2. Create some HTML code for each todo
+  3. Put the HTML on web page
+*/
+
+const todoList = ["wash dishes", "cook rice"];
+
+let todoListHTML = "";
+
+for (let i = 0; i < todoList.length; i++) {
+  const todo = todoList[i];
+  const html = `<p>${todo}</p>`;
+
+  todoListHTML += html;
+}
+
+console.log(todoListHTML);
 
 function addTodo() {
   let todoInput = document.querySelector(".todo-input");
   let todoInputElement = todoInput.value;
   console.log(todoInputElement);
 
+  todoList.push(todoInputElement);
+
+  todoInput.value = "";
   console.log(todoList);
 }
