bd8cf66b87e2c327c7477e4c105f629c271c6fdc
KATIN John <johnpkatin@gmail.com>
2025-11-30 21:34:03 +0000

    arrays and loops

---
+++ Lesson-11-arrays-and-loops/11-todo-list.js
@@ -0,0 +1,9 @@
+const todoList = [];
+
+function addTodo() {
+  let todoInput = document.querySelector(".todo-input");
+  let todoInputElement = todoInput.value;
+  console.log(todoInputElement);
+
+  console.log(todoList);
+}
